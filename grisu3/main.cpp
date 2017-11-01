b0f17c8593ce2b9e9ec42a18f9546bef96f23e8e
mazong1123 <mazong1123@gmail.com>
2017-11-01 12:30:27 +0800

    Added hit rate collecting support.

--- grisu3/main.cpp
+++ grisu3/main.cpp
@@ -5,12 +5,55 @@ using namespace std;
 
 int main()
 {
-    double value = 12;
-    int count = 3;
-    int dec;
-    int sign = 0;
-    wchar_t digits[51];
-    bool r = Grisu3::Run(value, count, &dec, &sign, digits);
+    for (int count = 17; count <= 17; ++count)
+    {
+        uint64_t success = 0;
+        uint64_t v = 0;
+        int dec;
+        int sign = 0;
+        wchar_t digits[51];
+        double value = 0;
+        for (; v < UINT64_MAX; ++v)
+        {
+            value = *((double*)&v);
+            if (((FPDOUBLE*)&value)->exp == 0x7FF || value == 0.0)
+            {
+                ++success;
+                continue;
+            }
+
+            if (value < 0)
+            {
+                value = -value;
+            }
+            
+            if (Grisu3::Run(value, count, &dec, &sign, digits))
+            {
+                ++success;
+            }
+
+            if (v % 10000000 == 0)
+            {
+                double rate = (double)success / (double)v * 100;
+                printf("The complete Grisu3 hit rate for precision count %d in %llu numbers is %.2f\n", count, v, rate);
+            }
+        }
+
+        value = *((double*)&v);
+        if (((FPDOUBLE*)&value)->exp == 0x7FF || value == 0.0)
+        {
+            ++success;
+            continue;
+        }
+
+        if (Grisu3::Run(value, count, &dec, &sign, digits))
+        {
+            ++success;
+        }
+
+        double rate = (double)success / (double)UINT64_MAX * 100;
+        printf("The complete Grisu3 hit rate for precision count %d is %.2f\n", count, rate);
+    }
 
     return 0;
 }
